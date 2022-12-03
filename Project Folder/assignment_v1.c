3921223ebcca25a1b1f358cf17f49417e02356ee
Dom Harvey <efydh1@nottingham.ac.uk>
2022-12-03 15:46:31 +0000

    project adjustments

--- Project Folder/assignment_v1.c
+++ Project Folder/assignment_v1.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //KEY:
-// // Real comment
-// //*** Missing/Unfinished Code
-// //err Error in Code
+// //       Real comment
+// //check  Temporary comment
+// //***    Missing/Unfinished Code
+// //err    Error in Code
 
-void main()
+int main()
 {
-    //*** EXECUTE READ SHAPE DATA
+    struct ShapeCoordSystem square, invTri, star, raTri, cross;
+    {
+        int numSides;
+        float x[numSides],y[numSides];
+        int z[numSides];
+    }
+
+    //check EXECUTE READ SHAPE DATA
     ReadShapeDataFunction();
 
     //*** ERROR CHECK
@@ -16,15 +25,19 @@ void main()
     //*** ERROR CHECK
     //*** EXECUTE SEND GCODE
     //*** ERROR CHECK
+
+    return 1;
 }
 
 int ReadShapeDataFunction()
 {
-    int testString = 0;
+    int totNumShapes = 0;
     //*** OPEN FILE
     FILE *fShapeData;
 
     fShapeData = fopen ("ShapeStrokeData.txt", "r");
+    
+    //*** ERROR CHECK - FILE OPEN?
 
     if (fShapeData == NULL)
     {
@@ -32,21 +45,20 @@ int ReadShapeDataFunction()
         return -1;
     }
 
-    fscanf(fShapeData, "%*s %d", &testString);
-    printf("%d", testString);
+    //*** GAIN TOTAL NUMBER OF SHAPES    
 
-
-    //*** ERROR CHECK - FILE OPEN?
-
-
-    //*** GAIN TOTAL NUMBER OF SHAPES
+    fscanf(fShapeData, "%*s %d", &totNumShapes);
 
 
     //*** ERROR CHECK - MORE SHAPES THAN EXPECTED? //err FIND WAY TO MAKE NUMBER OF SHAPES IRRELEVENT
 
 
     //*** READ SHAPE NAME AND NUMBER OF SIDES
+    char nameOfShape[20];
+    int shapeNumSides;
 
+    fscanf(fShapeData, "%s %d", nameOfShape, &shapeNumSides);
+    printf("%s", nameOfShape);
 
     //*** ERROR CHECK - NO NAME
 
@@ -55,6 +67,17 @@ int ReadShapeDataFunction()
 
 
     //*** PopulateShapeArrayFunction() //err USE STRUCTS?
+    if (strcmp(nameOfShape, "SQUARE"))
+    {
+        printf("Hello World");
+        //square.numSides = shapeNumSides;
+        //int i;
+        //for (i = 0; (i <= shapeNumSides); i++)
+        //{
+        //    fscanf(fShapeData, "%d %d %d", square.x[i], square.y[i], square.z[i]);
+        //}
+        //printf("%s", square.x);
+    }
 
 
     //*** CHECK FOR END OF FILE
@@ -65,5 +88,5 @@ int ReadShapeDataFunction()
 
     //*** ERROR CHECK - FILE CLOSED?
 
-
+    return 1;
 }
