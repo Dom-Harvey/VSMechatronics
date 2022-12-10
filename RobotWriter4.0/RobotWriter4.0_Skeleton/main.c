0141145e4993a81a01ab8395743407bcafb618bd
Dom Harvey <efydh1@nottingham.ac.uk>
2022-12-10 13:29:02 +0000

    commented

--- RobotWriter4.0/RobotWriter4.0_Skeleton/main.c
+++ RobotWriter4.0/RobotWriter4.0_Skeleton/main.c
@@ -9,46 +9,50 @@
 
 void SendCommands (char *buffer );
 
+/* STRUCTURE FOR SHAPE COORDINATE SYSTEM */
 struct ShapeCoordSystem
 {
-    char name[50];
-    int sides;
-    int x[100],y[100], z[100];
+    char name[50];                  // Name of shape
+    int instructions;               // Number of instructions to draw the shape
+    int x[100],y[100], z[100];      // Array to hold each of the drawing instructions
 };
 
+/* STRUCTURE FOR COMMAND SEQUENCE */
 struct CommandArray
 {
-    int numberOfCommands;
-    int drawFlag;
-    int xGrid[9], yGrid[9], shapeMarker[9];
-    char shape[20];
+    int numberOfCommands;                       // Number of commands within the drawing sequence file
+    int drawFlag;                               // Flag to indicate whether or not to draw the grid
+    int xGrid[9], yGrid[9], shapeMarker[9];     // arrays for grid coordinates - 9 for the 9 possible spaces
+    char shape[20];                             // read in name of shape for later use with shape markers
 };
 
-int ReadShapeDataFunction(int totalShapes, struct ShapeCoordSystem *);
-int DrawShapesFunction(int totalShapes, struct CommandArray *);
-int GCodeGenerationFunction(char buffer[100], struct CommandArray *, struct ShapeCoordSystem *);
+
+/* FUNCTION PROTOTYPING */
+int ReadShapeDataFunction(int totalShapes, struct ShapeCoordSystem *);                              // Hand total shapes, and the structure type pointer
+int DrawShapesFunction(int totalShapes, struct CommandArray *);                                     // Hand total shapes, and the structure type pointer
+int GCodeGenerationFunction(char buffer[100], struct CommandArray *, struct ShapeCoordSystem *);    // Hand the buffer and both structures
 
 int main()
 {
-    // Open file
+    /* Open file */
     FILE *fShapeData;
     fShapeData = fopen ("ShapeStrokeData.txt", "r");
-    // Reads total number of shapes and close file
+
+    /* Reads total number of shapes and close file */
     int totNumShapes;        
     fscanf(fShapeData, "%*s %d", &totNumShapes);
     fclose(fShapeData);
 
-    // Define ShapeCoordSystem for total shapes
+    /* Define ShapeCoordSystem and CommandArray */
     struct ShapeCoordSystem shapes[totNumShapes];
-    // Define CommandArray
-    struct CommandArray commandArray[1]; //err ONLY 1 - DON'T NEED ARRAY
+    struct CommandArray commandArray[1];            // Only generating one, but leaving in functionality for more than one command array in the event of using multiple machines
 
-    // Execute ReadShapeDataFunction
+    /* Execute function relating to reading in the shape data to memory */
     printf("Extracting Shape Data from file...\n");
     int readShapeDataOutput;
     readShapeDataOutput = ReadShapeDataFunction(totNumShapes, shapes);
 
-    // Check ShapeDataFunction output
+    /* Check ShapeDataFunction output for error trapping */
     switch(readShapeDataOutput)
     {
         case 1:
@@ -64,11 +68,12 @@ int main()
         printf("WARNING! Unexpected behaviour in Read Shape Data Function\n");
     }
 
-    // Execute DrawShapesFunction
+    /* Execute function relating to reading in the shape drawing instructions into the command array */
     printf("\nExtracting Drawing Commands from file...\n");
     int drawShapeOutput;
     drawShapeOutput = DrawShapesFunction(totNumShapes, commandArray);
 
+    /* Check DrawShapesFunctino output for error trapping */
     switch(drawShapeOutput)
     {
         case 1:
@@ -79,6 +84,9 @@ int main()
         break;
         case 3:
         printf("Shape Data file not closed correctly\n");
+        break;
+        case 4:
+        printf("Missing critical Shape Data in file. Check shape name format is SHAPE_NAME N");
         break;
         default:
         printf("WARNING! Unexpected behaviour in Read Shape Data Function\n");
@@ -113,7 +121,8 @@ int main()
 
     // These are sample commands to draw out some information - these are the ones you will be generating.
     //sprintf (buffer, "G0 X-13.41849 Y0.000\n");
-    // Execute GCodeGenerationFunction
+
+    /* Execute function relating to generating and sending G-Code */
     printf("\nGenerating G-Code...\n");
     int gCodeOutput;
     gCodeOutput =  GCodeGenerationFunction(buffer, commandArray, shapes);
@@ -121,7 +130,7 @@ int main()
     switch(gCodeOutput)
     {
         case 1:
-        printf("G-Code generated and send successfully\n");
+        printf("G-Code generated and sent successfully\n");
         break;
         case 2:
         printf("G-Code not generated successfully\n");
@@ -150,67 +159,71 @@ void SendCommands (char *buffer )
 
 int ReadShapeDataFunction(int totalShapes, struct ShapeCoordSystem *ptr1)
 {
-    // Open file
+    /* Open shape data file for reading and check it is opened successfully, then skip over number
+    of shapes because this was read in earlier*/
     FILE *fShapeData;
     fShapeData = fopen ("ShapeStrokeData.txt", "r");
-    
-    // Check file is opened successfully
+
     if (fShapeData == NULL)
     {
         return 2;
     }
-
-    //skip number of shapes (scanned in already)
     fscanf(fShapeData, "%*s %*d");
 
-    // Reads the next line to get a shape name and number of sides
+    /* Reads the next line to get a shape name and number of sides and returns an error if in the 
+    incorrect format */
     int j = 0;
     for (j = 0; (j < totalShapes); j++)
     {
         char nameOfShape[20];
-        int shapeNumSides;
+        int numInstructions = -1;   // Set intial number of instructions as -1 for error trapping (will never be -1)
 
-        fscanf(fShapeData, "%s %d", nameOfShape, &shapeNumSides);
-        ptr1->sides = shapeNumSides;
+        fscanf(fShapeData, "%s %d", nameOfShape, &numInstructions);
+        ptr1->instructions = numInstructions;
 
-        //*** ERROR CHECK - NO NUMBER OF SIDES?
+        if (numInstructions == -1) // Indicates no change in number of instructions
+        {
+            return 4;
+        }
 
-        // Populates a struct for each shape
+        /* Enters a for loop equal to the number of instructions just read, to populate the struct
+        with the x y and pen up/down info*/
         strcpy(ptr1->name, nameOfShape);
         int i = 0;
-        for (i = 0; (i < shapeNumSides); i++)
+        for (i = 0; (i < numInstructions); i++)
         {
             fscanf(fShapeData, "%d %d %d", &ptr1->x[i], &ptr1->y[i], &ptr1->z[i]);
         }
-        ptr1++;
+        ptr1++; // Move to oopulating the next struct
     }
-    // Closes the shape Data and checks that the file was closed successfully
-    fclose(fShapeData);
 
+    /* Close shape data file for reading and check it is closed successfully */
+    fclose(fShapeData);
     if (fShapeData == NULL)
     {
         return 3;
     }
-
+    
+    /* Function executed successfully if it reaches this point */
     return 1;
 }
 
 int DrawShapesFunction(int totalShapes, struct CommandArray *ptr2)
 {
-    //*** OPEN FILE
+    /* Open user selected file  for reading and check it is opened successfullyr*/
     FILE *fDrawShapes;
     char fileToOpen[50];
     printf("Please enter the file you would like to read in name.txt format: ");
     scanf("%s", fileToOpen);
     fDrawShapes = fopen(fileToOpen, "r");
     
-    //*** CHECK FILE WAS OPENED
     if (fDrawShapes == NULL)
     {
         return 2;
     }
 
-    //*** GAIN SIZE OF FILE
+    /* Gain length of file and check if the grid needs to be drawn, then minus 1 from length of
+    file to gain number of draw commands given */
     int counter = 0;
     char c;
     for(c = getc(fDrawShapes); c != EOF; c = getc(fDrawShapes))
@@ -222,12 +235,11 @@ int DrawShapesFunction(int totalShapes, struct CommandArray *ptr2)
     }
     rewind(fDrawShapes);
 
-    //*** SKIP FIRST LINE IF GRID
     char firstLine[20];
     fscanf(fDrawShapes, "%s %d", firstLine, &ptr2->drawFlag);
     counter--;
 
-    //*** ERROR CHECK - MORE DRAW COMMANDS THAN GRID SPACE?
+    /* If more commands are given than grid spaces, the command array will only draw in the first 9*/
     int totalSpace = 9;
     if (totalSpace < counter)
     {
@@ -237,12 +249,13 @@ int DrawShapesFunction(int totalShapes, struct CommandArray *ptr2)
 
     ptr2->numberOfCommands = counter;
 
-    //*** POPULATE COMMAND ARRAY
+    /* Populate the Command array with the x and y locations on the grid, and use the shape char to
+    collect the name of the shape and convert it to a number so it can all occupy the same array*/
     int i = 0;
-    int newMarker = totalShapes; //*** MAKE NOT MAGIC NUMBER
+    int newMarker = totalShapes;
     for (i = 0; (i <= counter); i++)
     {
-        fscanf(fDrawShapes, "%d %d %s", &ptr2->xGrid[i], &ptr2->yGrid[i], ptr2->shape); //err MAKE NOT BAD
+        fscanf(fDrawShapes, "%d %d %s", &ptr2->xGrid[i], &ptr2->yGrid[i], ptr2->shape);
         if (strcmp(ptr2->shape, "SQUARE") == 0)
         {
             ptr2->shapeMarker[i] = 0;
@@ -275,68 +288,71 @@ int DrawShapesFunction(int totalShapes, struct CommandArray *ptr2)
         }
     }
 
-    //*** CHECK FILE WAS CLOSED
+    /* Close draw commands file for reading and check it is closed successfully */
     if (fDrawShapes == NULL)
     {
         return 3;
     }
 
-
+    /* Function executed successfully if it reaches this point */
     return 1;
 }
 
 int GCodeGenerationFunction(char buffer[100], struct CommandArray *ptrMain, struct ShapeCoordSystem *ptrShape)
 {
-    //*** OPEN GCODE FILE
-    FILE *fGCode;
-    fGCode = fopen("GCode.txt", "w");
+    /* Open G-Code file for writing to and check it was opened successfully. Any line relating to this can be
+    commented out when sending commands to the robot. These lines are indicated by '//F '. */
 
-    //*** CHECK GCODE OPENED
-    if (fGCode == NULL)
-    {
-        return 2;
-    }
+    //F FILE *fGCode;
+    //F fGCode = fopen("GCode.txt", "w");
 
-    //*** INITIALISE
+ 
+    //F if (fGCode == NULL)
+    //F {
+    //F     return 2;
+    //F }
+
+    /* Initialise the pen properties */
     int penSpeed = 1000;
     int spindle = 3;
     int penUp = 0;
 
-    //fprintf(fGCode, "F%d M%d S%d", penSpeed, spindle, penUp);
+    //F fprintf(fGCode, "F%d M%d S%d", penSpeed, spindle, penUp);
     sprintf(buffer, "F%d\nM%d\nS%d", penSpeed, spindle, penUp);
     SendCommands(buffer);
     
-    //*** DRAW GRID
+    /* If the draw flag was given, draw the grid*/
     int gridSquare = 90;
     int numRowsColumns = 3;
     if (ptrMain->drawFlag == 1)
     {
-        //check zero pen
-        //fprintf(fGCode, "\nG0 X0 Y0\n");
+        // zero pen
+        //F fprintf(fGCode, "\nG0 X0 Y0\n");
         sprintf(buffer, "\nG0 X0 Y0\n");
         SendCommands(buffer);
-        Sleep(5000);
+
         //check draw square
-        //fprintf(fGCode, "\nS1000\nG1 X%d Y0\nG1 X%d Y%d\nG1 X0 Y%d\nG0 X0 Y0\n", gridSquare, gridSquare, -gridSquare, -gridSquare);
+        //F fprintf(fGCode, "\nS1000\nG1 X%d Y0\nG1 X%d Y%d\nG1 X0 Y%d\nG0 X0 Y0\n", gridSquare, gridSquare, -gridSquare, -gridSquare);
         sprintf(buffer, "\nS1000\nG1 X%d Y0\nG1 X%d Y%d\nG1 X0 Y%d\nG1 X0 Y0\n", gridSquare, gridSquare, -gridSquare, -gridSquare);
         SendCommands(buffer);
+
         //check draw columns
-        //fprintf(fGCode, "\nS0\nG0 X%d Y0\nS1000\nG1 X%d Y%d\nS0\nG0 X%d Y0\nS1000\nG1 X%d Y%d\n", gridSquare/numRowsColumns, gridSquare/numRowsColumns, -gridSquare, 2*gridSquare/numRowsColumns, 2*gridSquare/numRowsColumns, -gridSquare);
+        //F fprintf(fGCode, "\nS0\nG0 X%d Y0\nS1000\nG1 X%d Y%d\nS0\nG0 X%d Y0\nS1000\nG1 X%d Y%d\n", gridSquare/numRowsColumns, gridSquare/numRowsColumns, -gridSquare, 2*gridSquare/numRowsColumns, 2*gridSquare/numRowsColumns, -gridSquare);
         sprintf(buffer, "\nS0\nG0 X%d Y0\nS1000\nG1 X%d Y%d\nS0\nG0 X%d Y0\nS1000\nG1 X%d Y%d\n", gridSquare/numRowsColumns, gridSquare/numRowsColumns, -gridSquare, 2*gridSquare/numRowsColumns, 2*gridSquare/numRowsColumns, -gridSquare);
         SendCommands(buffer);
+
         //check draw rows
-        //fprintf(fGCode, "\nS0\nG0 X0 Y%d\nS1000\nG1 X%d Y%d\nS0\nG0 X0 Y%d\nS100\nG1 X%d Y%d\n", -gridSquare/numRowsColumns, gridSquare, -gridSquare/numRowsColumns, -2*gridSquare/numRowsColumns, gridSquare, -2*gridSquare/numRowsColumns);
+        //F fprintf(fGCode, "\nS0\nG0 X0 Y%d\nS1000\nG1 X%d Y%d\nS0\nG0 X0 Y%d\nS100\nG1 X%d Y%d\n", -gridSquare/numRowsColumns, gridSquare, -gridSquare/numRowsColumns, -2*gridSquare/numRowsColumns, gridSquare, -2*gridSquare/numRowsColumns);
         sprintf(buffer, "\nS0\nG0 X0 Y%d\nS1000\nG1 X%d Y%d\nS0\nG0 X0 Y%d\nS100\nG1 X%d Y%d\n", -gridSquare/numRowsColumns, gridSquare, -gridSquare/numRowsColumns, -2*gridSquare/numRowsColumns, gridSquare, -2*gridSquare/numRowsColumns);
         SendCommands(buffer);
 
     }
-    //*** GET LOCATION FROM COMMAND ARRAY
+
+    /* Get locations and shape from command array, zero the pen at the desired location, and generate the
+    sequence to be drawn for the shape, sending  each command to the buffer in a for loop */
     int i = 0;
     int iMax = ptrMain->numberOfCommands;
     int j = 0;
-    //err int occupiedArray[numRowsColumns][numRowsColumns];
-    //err memset(occupiedArray, 0, sizeof occupiedArray);
-    //err int relocateFlag;
     float newXZero;
     float newYZero;
     int toDrawShape;
@@ -352,58 +368,30 @@ int GCodeGenerationFunction(char buffer[100], struct CommandArray *ptrMain, stru
         newYZero = - yGridLoc * gridSquare/numRowsColumns + offset;
         toDrawShape = ptrMain->shapeMarker[i];
 
-        //*** ERROR CHECK - LOCATION OCCUPIED?
-        //errif (occupiedArray[ptrMain->xGrid[i]][ptrMain->yGrid[i]] == 1)
-        //err{
-        //err    relocateFlag = 1;
-        //err}
-        
-        //errwhile (relocateFlag == 1)
-        //err{
-        //err   printf("\nLocation occupied! Give new coordinates");
-        //err    printf("\nX Coord: ");
-        //err    scanf("%d", &xGridLoc);
-        //err    printf("\nY Coord: ");
-        //err    scanf("%d", &yGridLoc);
-        //*** ERROR CHECK - INPUTS VALID?
-
-        //err    if (occupiedArray[xGridLoc][yGridLoc] != 1)
-        //err    {
-        //err        relocateFlag = 0;
-        //err    }
-        //err}
-
-        // Zero pen at desired grid space
         //fprintf(fGCode,"\nS0\nG0 X%.2f Y%.2f\n", newXZero, newYZero);
         sprintf(buffer,"\nS0\nG0 X%.2f Y%.2f\n", newXZero, newYZero);
         SendCommands(buffer);
         // Generate sequence for current shape
-        for (j = 0; (j < ptrShape[toDrawShape].sides); j++)
+        for (j = 0; (j < ptrShape[toDrawShape].instructions); j++)
         {
             //fprintf(fGCode, "S%d\nG%d X%0.2f Y%.2f\n", 1000*ptrShape[toDrawShape].z[j], ptrShape[toDrawShape].z[j], newXZero + scalar*ptrShape[toDrawShape].x[j], newYZero + scalar*ptrShape[toDrawShape].y[j]);
             sprintf(buffer, "S%d\nG%d X%0.2f Y%.2f\n", 1000*ptrShape[toDrawShape].z[j],  ptrShape[toDrawShape].z[j], newXZero + scalar*ptrShape[toDrawShape].x[j], newYZero + scalar*ptrShape[toDrawShape].y[j]);
             SendCommands(buffer);
             Sleep(1000);
-
-
         }
-        // Marks location as occupied once full shape command sequence is formed
-        //err occupiedArray[xGridLoc][yGridLoc] = 1;
     }
-    // Zeroes pen now that sequence is complete
+    /* Zero the pen once all drawings have been completed, close the gcode if neccessary and */
     //fprintf(fGCode, "\nS0\nG0 X0 Y0\n");
     sprintf(buffer, "\nS0\nG0 X0 Y0\n");
     SendCommands(buffer);
     Sleep(5000);
 
-    // Closes G-Code
-    fclose(fGCode);
+    //F fclose(fGCode);
+    //F if (fGCode == NULL)
+    //F {
+    //F     return 3;
+    //F }
 
-    // Checs G-Code is closed
-    if (fGCode == NULL)
-    {
-        return 3;
-    }
-
+    /* Function executed successfully if it reaches this point */
     return 1;
 }
